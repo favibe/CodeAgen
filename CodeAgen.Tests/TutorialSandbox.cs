fe381debf350776b1815a4b0579fbb8074d16daa
favibe <arhovid@gmail.com>
2022-01-14 23:24:34 +0300

    Начал работу над документацией

---
+++ CodeAgen.Tests/TutorialSandbox.cs
@@ -0,0 +1,23 @@
+﻿using CodeAgen.Code;
+using CodeAgen.Code.Basic;
+using CodeAgen.Code.CodeTemplates;
+using CodeAgen.Outputs;
+using CodeAgen.Outputs.Entities;
+using Xunit;
+
+namespace CodeAgen.Tests
+{
+    public class TutorialSandbox
+    {
+        [Fact]
+        private void GenerateCode()
+        {
+            ICodeOutput output = new StandardCodeOutput();
+
+            var code = new CodeClass("ExampleClass", CodeAccessModifier.Internal, new CodeComment("Мой первый класс!"));
+            code.Build(output);
+            
+            var result = output.ToString();
+        }
+    }
+}
