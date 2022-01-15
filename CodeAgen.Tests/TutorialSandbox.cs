ca407a190804511ef2d4e82e2c99f7c1f21fd05f
favibe <arhovid@gmail.com>
2022-01-15 08:17:44 +0300

    Апдейт документации

--- CodeAgen.Tests/TutorialSandbox.cs
+++ CodeAgen.Tests/TutorialSandbox.cs
@@ -1,6 +1,8 @@
 ﻿using CodeAgen.Code;
 using CodeAgen.Code.Basic;
+using CodeAgen.Code.Basic.CodeNames;
 using CodeAgen.Code.CodeTemplates;
+using CodeAgen.Code.CodeTemplates.ClassMembers;
 using CodeAgen.Outputs;
 using CodeAgen.Outputs.Entities;
 using Xunit;
@@ -13,8 +15,18 @@ namespace CodeAgen.Tests
         private void GenerateCode()
         {
             ICodeOutput output = new StandardCodeOutput();
-
+            
             var code = new CodeClass("ExampleClass", CodeAccessModifier.Internal, new CodeComment("Мой первый класс!"));
+            //
+            //code.Field(new CodeField("float", "_index", accessModifier: CodeAccessModifier.Private,
+            //        isReadonly: true))
+            //    .Field(new CodeField("string", "_name", accessModifier: CodeAccessModifier.Private,
+            //        isReadonly: true));
+            //
+
+            code.AddUnit(new CodeLine("private readonly float _index"));
+            code.AddUnit(new CodeLine("private readonly float _name"));
+            
             code.Build(output);
             
             var result = output.ToString();
